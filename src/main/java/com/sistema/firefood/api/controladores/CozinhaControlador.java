061c8f8f83298546a30734fee5a809539b03970b
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2022-11-28 20:51:26 -0300

    feat - End Point deletar

--- src/main/java/com/sistema/firefood/api/controladores/CozinhaControlador.java
+++ src/main/java/com/sistema/firefood/api/controladores/CozinhaControlador.java
@@ -3,9 +3,11 @@ package com.sistema.firefood.api.controladores;
 
 import java.util.List;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sistema.firefood.domain.exceptions.EntidadeInvalidaException;
+import com.sistema.firefood.domain.exceptions.EntidadeNaoEncontradaException;
 import com.sistema.firefood.domain.modelos.Cozinha;
 import com.sistema.firefood.domain.servico.CozinhaServico;
 
@@ -50,6 +53,16 @@ public class CozinhaControlador {
 		}catch (EntidadeInvalidaException e) {
 			return ResponseEntity
 					.status(HttpStatus.UNPROCESSABLE_ENTITY).body(e.getMessage());
+		}
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public ResponseEntity<?> deletar(@PathVariable("id") Long id){
+		try {
+			cozinhaServico.deletar(id);
+			return ResponseEntity.status(200).build();
+		}catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.status(204).body(e.getMessage());
 		}
 	}
 	
