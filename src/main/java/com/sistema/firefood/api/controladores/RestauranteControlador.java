2b00a056ec993a0fd462bd9369fc05c22c09bdd8
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2022-11-29 13:46:24 -0300

    feat - GET listar

--- src/main/java/com/sistema/firefood/api/controladores/RestauranteControlador.java
+++ src/main/java/com/sistema/firefood/api/controladores/RestauranteControlador.java
@@ -1,8 +1,23 @@
 package com.sistema.firefood.api.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.sistema.firefood.domain.servico.RestauranteServico;
+
 @RestController
+@RequestMapping("/restaurantes")
 public class RestauranteControlador {
+	
+	@Autowired
+	private RestauranteServico restauranteServico;
+	
+	@GetMapping
+	public ResponseEntity<?> listar(){
+		return ResponseEntity.status(200).body(restauranteServico.listar());
+	}
 	
 }
