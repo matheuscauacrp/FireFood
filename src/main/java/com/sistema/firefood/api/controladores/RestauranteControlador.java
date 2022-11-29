b1827f57093c89d4977e94a55b886132baf1c304
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2022-11-29 13:40:20 -0300

    feat - Restaurante Controlador

--- src/main/java/com/sistema/firefood/api/controladores/RestauranteControlador.java
+++ src/main/java/com/sistema/firefood/api/controladores/RestauranteControlador.java
@@ -1,5 +1,8 @@
 package com.sistema.firefood.api.controladores;
 
-public class RestauranteControlador {
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class RestauranteControlador {
+	
 }
