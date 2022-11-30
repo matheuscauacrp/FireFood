c7ab925dd68d4a1f4f7eac532685284ea34c214f
Matheus Cauã Rocha Porto <matheuscrp1@hotmail.com>
2022-11-30 14:13:13 -0300

    feat - Méotodo atualizar Cozinha

--- src/main/java/com/sistema/firefood/api/controladores/CozinhaControlador.java
+++ src/main/java/com/sistema/firefood/api/controladores/CozinhaControlador.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,5 +65,15 @@ public class CozinhaControlador {
 			return ResponseEntity.status(204).body(e.getMessage());
 		}
 	}
+	@PutMapping("/atualizar")
+	public ResponseEntity<?> atualizar(@RequestBody Cozinha cozinha){
+		try {
+			return ResponseEntity
+				.status(HttpStatus.OK).body(cozinhaServico.salvar(cozinha));
+		}catch (EntidadeInvalidaException e) {
+			return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).body(e.getMessage());
+		}
+	}
+	
 	
 }
